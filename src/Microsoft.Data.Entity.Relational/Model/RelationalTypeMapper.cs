0f4fdc88b9914f58f75c2f26073bbdf87f5903a7
ajcvickers <ajcvickers@hotmail.com>
2014-05-13 13:31:18 -0700

    Varying double precision with time zone... (Moved SQL Server-specific type mapping into SQL Server assembly and added tests)
    Added tests for type mapping code checked in previously and at the same time moved the SQL Server specific mappings into the SQL Server assembly and checked that providers can override mappings.

--- src/Microsoft.Data.Entity.Relational/Model/RelationalTypeMapper.cs
+++ src/Microsoft.Data.Entity.Relational/Model/RelationalTypeMapper.cs
@@ -9,50 +9,36 @@ using Microsoft.Data.Entity.Relational.Utilities;
 
 namespace Microsoft.Data.Entity.Relational.Model
 {
-    // TODO: Some of this is SQL Server specific and should be moved into that class
     public class RelationalTypeMapper
     {
         // This dictionary is for invariant mappings from a sealed CLR type to a single
         // store type. If the CLR type is unsealed or if the mapping varies based on how the
         // type is used (e.g. in keys), then add custom mapping below.
+        // TODO: Linear lookup is probably faster than dictionary
         private readonly IDictionary<Type, RelationalTypeMapping> _simpleMappings = new Dictionary<Type, RelationalTypeMapping>()
             {
-                { typeof(int), new RelationalTypeMapping("int", DbType.Int32) },
-                { typeof(DateTime), new RelationalTypeMapping("datetime2", DbType.DateTime2) },
-                { typeof(Guid), new RelationalTypeMapping("uniqueidentifier", DbType.Guid) },
-                { typeof(bool), new RelationalTypeMapping("bit", DbType.Boolean) },
-                { typeof(byte), new RelationalTypeMapping("tinyint", DbType.Byte) },
-                { typeof(char), new RelationalTypeMapping("int", DbType.Int32) },
-                { typeof(double), new RelationalTypeMapping("float", DbType.Double) },
+                { typeof(int), new RelationalTypeMapping("integer", DbType.Int32) },
+                { typeof(DateTime), new RelationalTypeMapping("timestamp", DbType.DateTime) },
+                { typeof(bool), new RelationalTypeMapping("boolean", DbType.Boolean) },
+                { typeof(double), new RelationalTypeMapping("double precision", DbType.Double) },
                 { typeof(short), new RelationalTypeMapping("smallint", DbType.Int16) },
                 { typeof(long), new RelationalTypeMapping("bigint", DbType.Int64) },
-                { typeof(sbyte), new RelationalTypeMapping("smallint", DbType.SByte) },
                 { typeof(float), new RelationalTypeMapping("real", DbType.Single) },
-                { typeof(ushort), new RelationalTypeMapping("int", DbType.UInt16) },
-                { typeof(uint), new RelationalTypeMapping("bigint", DbType.UInt32) },
-                { typeof(ulong), new RelationalTypeMapping("numeric(20, 0)", DbType.UInt64) },
-                { typeof(DateTimeOffset), new RelationalTypeMapping("datetimeoffset", DbType.DateTimeOffset) },
+                { typeof(DateTimeOffset), new RelationalTypeMapping("timestamp with time zone", DbType.DateTimeOffset) },
             };
 
+        // TODO: What is the best size value for the base provider mapping?
         private readonly RelationalTypeMapping _nonKeyStringMapping
-            = new RelationalTypeMapping("nvarchar(max)", DbType.String);
+            = new RelationalSizedTypeMapping("varchar(4000)", DbType.AnsiString, 4000);
 
-        // TODO: It may be possible to increase 128 to 900, at least for SQL Server
+        // TODO: What is the best size value for the base provider mapping?
         private readonly RelationalTypeMapping _keyStringMapping
-            = new RelationalSizedTypeMapping("nvarchar(128)", DbType.String, 128);
-
-        private readonly RelationalTypeMapping _nonKeyByteArrayMapping
-            = new RelationalTypeMapping("varbinary(max)", DbType.Binary);
-
-        // TODO: It may be possible to increase 128 to 900, at least for SQL Server
-        private readonly RelationalTypeMapping _keyByteArrayMapping
-            = new RelationalSizedTypeMapping("varbinary(128)", DbType.Binary, 128);
+            = new RelationalSizedTypeMapping("varchar(128)", DbType.AnsiString, 128);
 
         private readonly RelationalTypeMapping _rowVersionMapping
             = new RelationalSizedTypeMapping("rowversion", DbType.Binary, 8);
 
-        private readonly RelationalDecimalTypeMapping _decimalMapping
-            = new RelationalDecimalTypeMapping(18, 2);
+        private readonly RelationalDecimalTypeMapping _decimalMapping = new RelationalDecimalTypeMapping(18, 2);
 
         // TODO: It would be nice to just pass IProperty into this method, but Migrations uses its own
         // store model for which there is no easy way to get an IProperty.
@@ -67,11 +53,19 @@ namespace Microsoft.Data.Entity.Relational.Model
             Check.NotNull(propertyType, "propertyType");
 
             // TODO: if specifiedType is non-null then parse it to create a type mapping
+            // TODO: Consider allowing Code First to specify an actual type mapping instead of just the string
+            // type since that would remove the need to parse the string.
 
             RelationalTypeMapping mapping;
             if (_simpleMappings.TryGetValue(propertyType, out mapping))
             {
                 return mapping;
+            }
+
+            if (propertyType == typeof(decimal))
+            {
+                // TODO: If scale/precision have been configured for the property, then create parameter appropriately
+                return _decimalMapping;
             }
 
             if (propertyType == typeof(string))
@@ -83,27 +77,12 @@ namespace Microsoft.Data.Entity.Relational.Model
                 return _nonKeyStringMapping;
             }
 
-            if (propertyType == typeof(byte[]))
+            if (propertyType == typeof(byte[]) && isConcurrencyToken)
             {
-                if (isKey)
-                {
-                    return _keyByteArrayMapping;
-                }
-
-                if (isConcurrencyToken)
-                {
-                    return _rowVersionMapping;
-                }
-                return _nonKeyByteArrayMapping;
+                return _rowVersionMapping;
             }
 
-            if (propertyType == typeof(decimal))
-            {
-                // TODO: If scale/precision have been configured for the property, then create parameter appropriately
-                return _decimalMapping;
-            }
-
-            //// TODO: Consider TimeSpan mapping
+            // TODO: Consider TimeSpan mapping
 
             throw new NotSupportedException(Strings.FormatUnsupportedType(storageName, propertyType.Name));
         }
--- src/Microsoft.Data.Entity.SqlServer/SqlServerTypeMapper.cs
+++ src/Microsoft.Data.Entity.SqlServer/SqlServerTypeMapper.cs
@@ -1,11 +1,81 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Data;
 using Microsoft.Data.Entity.Relational.Model;
 
 namespace Microsoft.Data.Entity.SqlServer
 {
     public class SqlServerTypeMapper : RelationalTypeMapper
     {
+        // This dictionary is for invariant mappings from a sealed CLR type to a single
+        // store type. If the CLR type is unsealed or if the mapping varies based on how the
+        // type is used (e.g. in keys), then add custom mapping below.
+        // TODO: Linear lookup is probably faster than dictionary
+        private readonly IDictionary<Type, RelationalTypeMapping> _simpleMappings = new Dictionary<Type, RelationalTypeMapping>()
+            {
+                { typeof(int), new RelationalTypeMapping("int", DbType.Int32) },
+                { typeof(DateTime), new RelationalTypeMapping("datetime2", DbType.DateTime2) },
+                { typeof(Guid), new RelationalTypeMapping("uniqueidentifier", DbType.Guid) },
+                { typeof(char), new RelationalTypeMapping("int", DbType.Int32) },
+                { typeof(bool), new RelationalTypeMapping("bit", DbType.Boolean) },
+                { typeof(byte), new RelationalTypeMapping("tinyint", DbType.Byte) },
+                { typeof(double), new RelationalTypeMapping("float", DbType.Double) },
+                { typeof(sbyte), new RelationalTypeMapping("smallint", DbType.SByte) },
+                { typeof(ushort), new RelationalTypeMapping("int", DbType.UInt16) },
+                { typeof(uint), new RelationalTypeMapping("bigint", DbType.UInt32) },
+                { typeof(ulong), new RelationalTypeMapping("numeric(20, 0)", DbType.UInt64) },
+                { typeof(DateTimeOffset), new RelationalTypeMapping("datetimeoffset", DbType.DateTimeOffset) },
+            };
+
+        private readonly RelationalTypeMapping _nonKeyStringMapping
+            = new RelationalTypeMapping("nvarchar(max)", DbType.String);
+
+        // TODO: It may be possible to increase 128 to 900, at least for SQL Server
+        private readonly RelationalTypeMapping _keyStringMapping
+            = new RelationalSizedTypeMapping("nvarchar(128)", DbType.String, 128);
+
+        private readonly RelationalTypeMapping _nonKeyByteArrayMapping
+            = new RelationalTypeMapping("varbinary(max)", DbType.Binary);
+
+        // TODO: It may be possible to increase 128 to 900, at least for SQL Server
+        private readonly RelationalTypeMapping _keyByteArrayMapping
+            = new RelationalSizedTypeMapping("varbinary(128)", DbType.Binary, 128);
+
+        public override RelationalTypeMapping GetTypeMapping(
+            string specifiedType, string storageName, Type propertyType, bool isKey, bool isConcurrencyToken)
+        {
+            RelationalTypeMapping mapping;
+            if (_simpleMappings.TryGetValue(propertyType, out mapping))
+            {
+                return mapping;
+            }
+
+            if (propertyType == typeof(string))
+            {
+                if (isKey)
+                {
+                    return _keyStringMapping;
+                }
+                return _nonKeyStringMapping;
+            }
+
+            if (propertyType == typeof(byte[]))
+            {
+                if (isKey)
+                {
+                    return _keyByteArrayMapping;
+                }
+
+                if (!isConcurrencyToken)
+                {
+                    return _nonKeyByteArrayMapping;
+                }
+            }
+
+            return base.GetTypeMapping(specifiedType, storageName, propertyType, isKey, isConcurrencyToken);
+        }
     }
 }
